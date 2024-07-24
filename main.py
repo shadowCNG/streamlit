fdba0dc0e49c3f8b3cc77ff98a03cad79eb89731
shadow <52004377+shadowCNG@users.noreply.github.com>
2024-07-24 21:18:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from gdata import getData
+import requests
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,6 +12,17 @@ def hello():
 def info():
     system_info = getData()
     return system_info
+    
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        response.raise_for_status()
+        public_ip = response.json()['ip']
+    except requests.RequestException as e:
+        print(f"Error fetching public IP: {e}")
+        public_ip = None
+    return public_ip
 
 if __name__ == '__main__':
+    os.write(1, f"{get_public_ip()}\n".encode())
     app.run(port=8831, debug=True, use_reloader=False)
