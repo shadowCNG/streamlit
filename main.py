61183620a137aed31de11d93ddef3b9880852021
shadow <52004377+shadowCNG@users.noreply.github.com>
2024-07-24 21:09:47 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from gdata import getData
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/info')
+def info():
+    system_info = getData()
+    return system_info
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
