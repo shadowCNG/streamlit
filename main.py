419e171b7d3086da35ae4e47dbd53a5c5242e5c7
shadow <52004377+shadowCNG@users.noreply.github.com>
2024-07-25 12:26:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,21 @@ dirs, files = list_files_and_dirs(st.session_state.current_dir)
 
 # 显示目录
 selected_dir = st.sidebar.selectbox('选择目录', ['/'] + dirs)
-if selected_dir != '/':
+if selected_dir == '/':
+    st.session_state.current_dir = os.getcwd()
+else:
     st.session_state.current_dir = os.path.join(st.session_state.current_dir, selected_dir)
-    st.sidebar.write(f'当前目录: {st.session_state.current_dir}')
+st.sidebar.write(f'当前目录: {st.session_state.current_dir}')
+
+# 文件上传按钮放在左侧区域
+uploaded_file = st.sidebar.file_uploader('上传文件', type=['txt', 'py', 'md', 'jpg', 'png'])
+if uploaded_file is not None:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file.flush()
+        shutil.move(tmp_file.name, os.path.join(st.session_state.current_dir, uploaded_file.name))
+        st.sidebar.write(f'文件 {uploaded_file.name} 已上传到 {st.session_state.current_dir}')
+        st.sidebar.success('文件已上传')
 
 # 显示文件
 selected_file = st.sidebar.selectbox('选择文件', files)
@@ -41,36 +53,24 @@ if selected_file:
 
     # 文件内容
     file_content = open(file_path, 'r').read()
-    st.text_area('文件内容', value=file_content, height=300)
+    edited_content = st.text_area('文件内容', value=file_content, height=300)
+
+    # 保存修改
+    if st.button('保存修改'):
+        with open(file_path, 'w') as file:
+            file.write(edited_content)
+        st.write('文件已保存')
 
     # 文件操作
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
 
     with col1:
-        if st.button('编辑文件'):
-            new_content = st.text_area('编辑文件内容', value=file_content, height=300)
-            if st.button('保存'):
-                with open(file_path, 'w') as file:
-                    file.write(new_content)
-                st.write('文件已保存')
-
-    with col2:
         if st.button('下载文件'):
             with open(file_path, 'rb') as file:
                 st.download_button('下载', file.read(), file_name=selected_file)
 
-    with col3:
+    with col2:
         if st.button('删除文件'):
             os.remove(file_path)
             st.write(f'文件 {selected_file} 已删除')
             st.sidebar.success('文件已删除')
-
-# 文件上传
-uploaded_file = st.file_uploader('上传文件', type=['txt', 'py', 'md', 'jpg', 'png'])
-if uploaded_file is not None:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file.flush()
-        shutil.move(tmp_file.name, os.path.join(st.session_state.current_dir, uploaded_file.name))
-        st.write(f'文件 {uploaded_file.name} 已上传到 {st.session_state.current_dir}')
-        st.sidebar.success('文件已上传')
